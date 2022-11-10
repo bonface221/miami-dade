348ec4cb10e1711781f9ce88e7f0519c569b5090
bonface221 <mainaboniface221@gmail.com>
2022-11-10 20:51:07 +0300

    ft:added a footer template

--- base/models.py
+++ base/models.py
@@ -3,3 +3,17 @@ from django.db import models
 
 
 # Create your models here.
+# this is the model for the first search here is the link to the searched website 'https://onlineservices.miami-dadeclerk.com/officialrecords/StandardSearch.aspx'
+class OfficialSearch(models.Model):
+    Clerks_File_No = models.CharField(max_length=255)
+    Doc_Type = models.CharField(max_length=255)
+    Rec_Date = models.DateField()
+    Rec_Book_or_Page = models.CharField(max_length=255)
+    Plat_Book_or_Page = models.CharField(max_length=255)
+    Blk=models.DecimalField(max_digits=6,decimal_places=2,null=True)
+    Legal = models.CharField(max_length=255)
+    Misc_Ref = models.CharField(max_length=255)
+    First_Party_Code = models.CharField(max_length=255)
+    Second_Party_Code = models.CharField(max_length=255)
+
+
--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
@@ -5,6 +6,9 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from .forms import UserForm
 from django.contrib import messages
+
+
+
 # from .scrapper import scrapper
 # from .selenium import selenium_func
 # Create your views here.
@@ -55,15 +59,164 @@ def logoutUser(request):
 
 @login_required(login_url='login')
 def homePage(request):
-
+    '''
+    this is the homepage section
+    '''
     context = dict()
     return render(request,'base/home.html',context)
 
-@login_required(login_url='login')
+
+
+
+@csrf_exempt
 def showCases(request):
+    
+    '''
+    check if there is a post request from our scrapper and tackle the rest of the work
+    '''
+    sample_data = [
+        {
+            "Clerk_File_No": "2022 R 831976",
+            "Doc_Type": "LIS",
+            "Rec_Date": "11/1/2022",
+            "Rec_Book_or_Page": "33447 / 92",
+            "Plat_Book_or_Page": "149/220",
+            'Blk': '5.0',
+            'Legal': "LOT 2A",
+            "Misc_Ref": "2022-020673-CA-01 LISPCV",
+            "First_Party_Code": 'HSBC BANK USA NA (D)',
+            "Second_Party_Code": "LARGAESPADA SILVIA"
+
+        },
+        {
+            "Clerk_File_No": "2022 R 831976",
+            "Doc_Type": "LIS",
+            "Rec_Date": "11/1/2022",
+            "Rec_Book_or_Page": "33447 / 92",
+            "Plat_Book_or_Page": "149/220",
+            'Blk': '5.0',
+            'Legal': "LOT 2A",
+            "Misc_Ref": "2022-020673-CA-01 LISPCV",
+            "First_Party_Code": 'HSBC BANK USA NA (D)',
+            "Second_Party_Code": "LARGAESPADA SILVIA"
+
+        },
+        {
+            "Clerk_File_No": "2022 R 831976",
+            "Doc_Type": "LIS",
+            "Rec_Date": "11/1/2022",
+            "Rec_Book_or_Page": "33447 / 92",
+            "Plat_Book_or_Page": "149/220",
+            'Blk': '5.0',
+            'Legal': "LOT 2A",
+            "Misc_Ref": "2022-020673-CA-01 LISPCV",
+            "First_Party_Code": 'HSBC BANK USA NA (D)',
+            "Second_Party_Code": "LARGAESPADA SILVIA"
+
+        },
+        {
+            "Clerk_File_No": "2022 R 831976",
+            "Doc_Type": "LIS",
+            "Rec_Date": "11/1/2022",
+            "Rec_Book_or_Page": "33447 / 92",
+            "Plat_Book_or_Page": "149/220",
+            'Blk': '5.0',
+            'Legal': "LOT 2A",
+            "Misc_Ref": "2022-020673-CA-01 LISPCV",
+            "First_Party_Code": 'KENDALE LAKES NORTH SECTION NINE HOMEOWNERS ASSN INC (R)',
+            "Second_Party_Code": "HSBC BANK USA NA"
+
+        },
+        {
+            "Clerk_File_No": "2022 R 831976",
+            "Doc_Type": "LIS",
+            "Rec_Date": "11/1/2022",
+            "Rec_Book_or_Page": "33447 / 92",
+            "Plat_Book_or_Page": "149/220",
+            'Blk': '5.0',
+            'Legal': "LOT 2A",
+            "Misc_Ref": "2022-020673-CA-01 LISPCV",
+            "First_Party_Code": 'LARGAESPADA SILVIA (R)',
+            "Second_Party_Code": "HSBC BANK USA NA"
+        },
+        {
+            "Clerk_File_No": "2022 R 831982",
+            "Doc_Type": "LIS",
+            "Rec_Date": "11/1/2022",
+            "Rec_Book_or_Page": "33447 / 101",
+            "Plat_Book_or_Page": "96/110",
+            'Blk': '17.0',
+            'Legal': "LOT 2",
+            "Misc_Ref": "2022-020677-CA-01 LISPCV",
+            "First_Party_Code": 'LARGAESPADA SILVIA (R)',
+            "Second_Party_Code": "HSBC BANK USA NA"
+        },
+        {
+            "Clerk_File_No": "2022 R 831982",
+            "Doc_Type": "LIS",
+            "Rec_Date": "11/1/2022",
+            "Rec_Book_or_Page": "33447 / 101",
+            "Plat_Book_or_Page": "96/110",
+            'Blk': '17.0',
+            'Legal': "LOT 2",
+            "Misc_Ref": "2022-020677-CA-01 LISPCV",
+            "First_Party_Code": 'LARGAESPADA SILVIA (R)',
+            "Second_Party_Code": "HSBC BANK USA NA"
+        },
+        {
+            "Clerk_File_No": "2022 R 832023",
+            "Doc_Type": "LIS",
+            "Rec_Date": "11/1/2022",
+            "Rec_Book_or_Page": "33447 / 207",
+            "Plat_Book_or_Page": "89/100",
+            'Blk': '2.0',
+            'Legal': "LOT 2",
+            "Misc_Ref": "2022-020681-CA-01 LISPCV",
+            "First_Party_Code": 'LARGAESPADA SILVIA (R)',
+            "Second_Party_Code": "HSBC BANK USA NA"
+        },
+        {
+            "Clerk_File_No": "2022 R 832023",
+            "Doc_Type": "LIS",
+            "Rec_Date": "11/1/2022",
+            "Rec_Book_or_Page": "33447 / 207",
+            "Plat_Book_or_Page": "89/100",
+            'Blk': '2.0',
+            'Legal': "LOT 2",
+            "Misc_Ref": "2022-020681-CA-01 LISPCV",
+            "First_Party_Code": 'LARGAESPADA SILVIA (R)',
+            "Second_Party_Code": "HSBC BANK USA NA"
+        },
+        {
+            "Clerk_File_No": "2022 R 833199",
+            "Doc_Type": "LIS",
+            "Rec_Date": "11/1/2022",
+            "Rec_Book_or_Page": "33447 / 3271",
+            "Plat_Book_or_Page": "118/180",
+            'Blk': '75.0',
+            'Legal': "LOT 10",
+            "Misc_Ref": "2022-020662-CA-01 LISPCV",
+            "First_Party_Code": 'LARGAESPADA SILVIA (R)',
+            "Second_Party_Code": "HSBC BANK USA NA"
+        },
+        {
+            "Clerk_File_No": "2022 R 833199",
+            "Doc_Type": "LIS",
+            "Rec_Date": "11/1/2022",
+            "Rec_Book_or_Page": "33447 / 3271",
+            "Plat_Book_or_Page": "118/180",
+            'Blk': '75.0',
+            'Legal': "LOT 10",
+            "Misc_Ref": "2022-020662-CA-01 LISPCV",
+            "First_Party_Code": 'LARGAESPADA SILVIA (R)',
+            "Second_Party_Code": "HSBC BANK USA NA"
+        },
+    ]
+
+
     if request.method == 'POST':
         data= request.POST
-        print(data)
-    context= dict()
+       
+    context= dict(data=sample_data)
 
     return render(request,'base/showCases.html',context)
