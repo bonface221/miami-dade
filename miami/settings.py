59befe8c924515f50f2bccbdca8907d009d78faa
bonface221 <mainaboniface221@gmail.com>
2022-11-15 06:34:30 +0300

    ft:added a developement branch to prevent overiding master

--- miami/settings.py
+++ miami/settings.py
@@ -88,22 +88,22 @@ WSGI_APPLICATION = 'miami.wsgi.application'
 # That's all also remmember to use the scrapper to populate the database and make sure the server is running..... python manage.py runserver
 # the scrapper is using virtual env so it is a matter of re-installing from the requirements .txt file ....its using pip ....so run this command $pip uninstall -r requirements.txt
 # Run the spiders using $spider crawl miami and the next one is spider crawl advanced .. miami and advanced are names of spiders.....
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# This is using Postgress uncomment to use it!!!!!!!!!!!!!!!!!!!!!
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('NAME'),
-#         'USER': config('USER'),
-#         'PASSWORD': config('PASSWORD'),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+# This is using Postgress uncomment to use it!!!!!!!!!!!!!!!!!!!!!
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+    }
+}
 
 
 # Password validation
